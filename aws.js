83a34409589700533764220237a3042c86c6f0ef
Jonathan Yaniv <johnitravolta@gmail.com>
2016-04-24 02:43:25 +0300

    Added AWS utils file. Website now supports uploading directly to S3 using pre-signed urls.

---
+++ aws.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function AWSUtils() {
+  function upload(file, signed_url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", signed_url);
+    xhr.setRequestHeader("x-amz-acl", "public-read");
+    xhr.send(file);
+  }
+
+  function sign_request(file, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/sign-url?fileName=" + file.name + "&fileType=" + file.type);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        callback(response);
+      }
+    };
+
+    xhr.send();
+  }
+
+  return {
+    upload: upload,
+    sign_request: sign_request
+  };
+}
+
+module.exports = AWSUtils();
--- index.js
+++ index.js
@@ -6,6 +6,7 @@
   require("./bower_components/bootstrap/dist/css/bootstrap.min.css");
   require("fetch");
   var grid = require("./Grid");
+  var awsUtils = require("./aws");
 
   fetch("./photos.json")
       .then(function (res) {
@@ -18,4 +19,17 @@
         console.log("Error parsing fetch results");
         console.error(ex);
       });
+
+  var imageElement = document.getElementById("image");
+  imageElement.onchange = function () {
+    var file = imageElement.files[0];
+    if (!file) {
+      return;
+    }
+
+    awsUtils.sign_request(file, function (res) {
+      awsUtils.upload(file, res.signed_url);
+      console.log(res);
+    });
+  }
 })();
